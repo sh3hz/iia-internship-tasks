3f8d1c4df0ecca036cb33266f1cb471497557233
sh3hz <shehax21@gmail.com>
2021-04-14 17:23:03 +0530

    Captcha Validation

--- Js validation/Inline/script.js
+++ Js validation/Inline/script.js
@@ -188,3 +188,19 @@ for(var i = 0; i < options.length; i++) {
     el.value = opt;
     select.appendChild(el);
 }
+
+//Captcha Validation
+function submitUserForm() {
+    var response = grecaptcha.getResponse();
+    if(response.length == 0) {
+        document.getElementById('g-recaptcha-error').innerHTML = '<span style="color:red;">This field is required.</span>';
+        return false;
+    }
+    else{
+			alert("Account created successfully");
+	}
+}
+ 
+function verifyCaptcha() {
+    document.getElementById('g-recaptcha-error').innerHTML = '';
+}
